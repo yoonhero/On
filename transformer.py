b6fa722872846d3af14ca1ca5e6b2c45d44c4f8d
yoonhero <yoonhero06@naver.com>
2022-08-04 17:09:30 +0900

    feat: Scaled Dot-Product Attention

--- transformer.py
+++ transformer.py
@@ -50,6 +50,41 @@ class PositionalEncoding(tf.keras.layers.Layer):
         return inputs + self.pos_encoding[:, :tf.shape(inputs)[1], :]
     
 
+
+# Scaled Dot-Product Attention
+def scaled_dot_product_attention(query, key, value, mask):
+    # query Dimension : d_model/num_heads 
+    # key Dimension : d_model/num_heads
+    # value Dimension : d_model/num_heads
+    # padding_mask : (batch_size, 1, 1, key Dimension)
+
+    # Q와 K의 곱. Attention Score Matrix 
+    # transpose is 전치행렬
+    matmul_qk = tf.matmul(query, key, transpose_b=True)
+
+    # Scailing
+    # divide root(dk=depth)
+    depth = tf.cast(tf.shape(key)[-1], tf.float32)
+    logits = matmul_qk / tf.math.sqrt(depth)
+
+    # Masking. Insert very small negative number in Attention Score Matrix to mask
+    # Very Small -> Softmax and 0
+    if mask is not None:
+        logits += (mask * -1e9)
+        
+    
+    # Softmax perform as last dimension of Key
+    # Attention Weight: (batch_size, num_heads, last query Dimension, key last Dimension)
+    attention_weights = tf.nn.softmax(logits, axis=-1)
+
+
+    # output : (batch_size, num_heads, query last Dimension, d_model/num_heads)
+    output = tf.matmul(attention_weights, value)
+
+    return output, attention_weights
+
+
+
 if __name__ == "__main__":
     # 문장의 길이 50, 임베딩 벡터의 차원 128
     sample_pos_encoding = PositionalEncoding(50, 128)
@@ -60,3 +95,22 @@ if __name__ == "__main__":
     plt.ylabel('Position')
     plt.colorbar()
     plt.show()
+
+
+    # 임의의 Query, Key, Value인 Q, K, V 행렬 생성
+    np.set_printoptions(suppress=True)
+    temp_k = tf.constant([[10,0,0],
+                        [0,10,0],
+                        [0,0,10],
+                        [0,0,10]], dtype=tf.float32)  # (4, 3)
+
+    temp_v = tf.constant([[   1,0],
+                        [  10,0],
+                        [ 100,5],
+                        [1000,6]], dtype=tf.float32)  # (4, 2)
+    temp_q = tf.constant([[0, 10, 0]], dtype=tf.float32)  # (1, 3)
+
+    # 함수 실행
+    temp_out, temp_attn = scaled_dot_product_attention(temp_q, temp_k, temp_v, None)
+    print(temp_attn) # 어텐션 분포(어텐션 가중치의 나열)
+    print(temp_out) # 어텐션 값 
