407cb3fd69a5d32661867e383f6a917ac7b1bfea
yoonhero <yoonhero06@naver.com>
2022-08-05 10:21:29 +0900

    refactor: Predict Module

--- utils.py
+++ utils.py
@@ -13,46 +13,58 @@ import time
 import datetime
 
 
+class predict():
+    def __init__(self, model, tokenizer, START_TOKEN, END_TOKEN, MAX_LENGTH):
+        self.model = model
+        
+        self.tokenizer = tokenizer
+        self.START_TOKEN = START_TOKEN
+        self.END_TOKEN = END_TOKEN
 
-def evaluate(model, sentence, tokenizer, START_TOKEN, END_TOKEN, MAX_LENGTH):
-    # 입력 문장에 대한 전처리
-    sentence = preprocess_sentence(sentence)
-
-    # 입력 문장에 시작 토큰과 종료 토큰을 추가
-    sentence = tf.expand_dims(
-        START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
-
-    output = tf.expand_dims(START_TOKEN, 0)
-
-    # 디코더의 예측 시작
-    for i in range(MAX_LENGTH):
-        predictions = model(inputs=[sentence, output], training=False)
-
-        # 현재 시점의 예측 단어를 받아온다.
-        predictions = predictions[:, -1:, :]
-        predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
-
-        # 만약 현재 시점의 예측 단어가 종료 토큰이라면 예측을 중단
-        if tf.equal(predicted_id, END_TOKEN[0]):
-            break
-
-        # 현재 시점의 예측 단어를 output(출력)에 연결한다.
-        # output은 for문의 다음 루프에서 디코더의 입력이 된다.
-        output = tf.concat([output, predicted_id], axis=-1)
-
-    # 단어 예측이 모두 끝났다면 output을 리턴.
-    return tf.squeeze(output, axis=0)
+        self.MAX_LENGTH = MAX_LENGTH
 
 
-def predict(model, sentence, tokenizer, START_TOKEN, END_TOKEN, MAX_LENGTH):
-  prediction = evaluate(model=model, sentence=sentence, tokenizer=tokenizer, START_TOKEN=START_TOKEN, END_TOKEN=END_TOKEN, MAX_LENGTH=MAX_LENGTH)
+    def evaluate(self, sentence):
+        # 입력 문장에 대한 전처리
+        sentence = preprocess_sentence(sentence)
 
-  # prediction == 디코더가 리턴한 챗봇의 대답에 해당하는 정수 시퀀스
-  # tokenizer.decode()를 통해 정수 시퀀스를 문자열로 디코딩.
-  predicted_sentence = tokenizer.decode(
-      [i for i in prediction if i < tokenizer.vocab_size])
+        # 입력 문장에 시작 토큰과 종료 토큰을 추가
+        sentence = tf.expand_dims(
+            self.START_TOKEN + self.tokenizer.encode(sentence) + self.END_TOKEN, axis=0)
 
-  return predicted_sentence
+        output = tf.expand_dims(self.START_TOKEN, 0)
+
+        # 디코더의 예측 시작
+        for i in range(self.MAX_LENGTH):
+            predictions = self.model(inputs=[sentence, output], training=False)
+
+            # 현재 시점의 예측 단어를 받아온다.
+            predictions = predictions[:, -1:, :]
+            predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+            # 만약 현재 시점의 예측 단어가 종료 토큰이라면 예측을 중단
+            if tf.equal(predicted_id, self.END_TOKEN[0]):
+                break
+
+            # 현재 시점의 예측 단어를 output(출력)에 연결한다.
+            # output은 for문의 다음 루프에서 디코더의 입력이 된다.
+            output = tf.concat([output, predicted_id], axis=-1)
+
+        # 단어 예측이 모두 끝났다면 output을 리턴.
+        return tf.squeeze(output, axis=0)
+
+    def __call__(self, sentence):
+        if sentence == None:
+            return 
+
+        prediction = self.evaluate(sentence)
+
+        # prediction == 디코더가 리턴한 챗봇의 대답에 해당하는 정수 시퀀스
+        # tokenizer.decode()를 통해 정수 시퀀스를 문자열로 디코딩.
+        predicted_sentence = self.tokenizer.decode(
+            [i for i in prediction if i < self.tokenizer.vocab_size])
+
+        return predicted_sentence
 
 
 
